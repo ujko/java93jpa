cbc4e23fdd4e2e538eb480d87c011d6920f991d8
uniko <uio6398@gmail.com>
2021-10-03 11:28:08 +0200

    two hibernates

--- src/main/java/Main2.java
+++ src/main/java/Main2.java
@@ -1,4 +1,4 @@
-import models.twoObjToOneTable.embedded.Person2;
+import models.Person;
 import models.twoTablesToOneObj.Employee;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
@@ -6,9 +6,14 @@ import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.query.Query;
 
+import java.util.List;
+
 public class Main2 {
     private SessionFactory factory;
     private Session session;
+
+    private SessionFactory factoryTest;
+    private Session sessionTest;
 
     private void startSession() {
         factory = new Configuration()
@@ -22,18 +27,30 @@ public class Main2 {
         session.close();
     }
 
+    private void startSessionTest() {
+        factoryTest = new Configuration()
+                .configure("hibernate2.cfg.xml")
+                .buildSessionFactory();
+        sessionTest = factoryTest.openSession();
+    }
+
+    private void stopSessionTest() {
+        factoryTest.close();
+        sessionTest.close();
+    }
+
     private void doSth() {
         startSession();
-        Employee employee = new Employee();
-        employee.setFirstName("Tadeusz");
-        employee.setLastName("Nowak");
-        employee.setCity("Lodz");
-        employee.setStreet("Piotrkowska");
-        Transaction tx = session.beginTransaction();
-        session.save(employee);
-        tx.commit();
+        Query<Person> query = session.createQuery("select p from Person p");
+        List<Person> personList = query.getResultList();
         stopSession();
+        startSessionTest();
+        Transaction tx = sessionTest.beginTransaction();
+        personList.forEach(x -> sessionTest.save(x));
+        tx.commit();
+        stopSessionTest();
     }
+    
     public static void main(String[] args) {
         new Main2().doSth();
     }
